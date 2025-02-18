6547c54587992846547a8e533d672b5e39aeffaf
Husnain <daimshah784@gmail.com>
2025-02-18 07:49:55 +0300

    solved: pyramid problem

--- AlgoCasts/exercises/pyramid/index.js
+++ AlgoCasts/exercises/pyramid/index.js
@@ -13,7 +13,27 @@
 //       '  #  '
 //       ' ### '
 //       '#####'
+//   pyramid(4)
+//       '   #   '
+//       '  ###  '
+//       ' ##### '
+//       '#######'
 
-function pyramid(n) {}
+function pyramid(n) {
+	const totalLength = n + (n - 1);
+	for (let i = 1; i <= n; i++) {
+		let str = '';
+		const maxRowCharacter = i + (i - 1);
+		const paddingEachSide = (totalLength - maxRowCharacter) / 2;
+		for (let j = 1; j <= totalLength; j++) {
+			if (j > paddingEachSide && j <= maxRowCharacter + paddingEachSide) {
+				str += '#';
+			} else {
+				str += ' ';
+			}
+		}
+		console.log(str);
+	}
+}
 
 module.exports = pyramid;
